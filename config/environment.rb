5f9879c8b0b07869c86ca1471dee1222adbf5c9f
reevesalic <reevesalic@gmail.com>
2020-07-31 16:02:02 -0400

    Initial file setup

---
+++ config/environment.rb
@@ -0,0 +1,15 @@
+# require '../lib/version.rb'
+
+module RubyProject
+  class Error < StandardError; end
+  # Your code goes here...
+end
+
+
+require 'dotenv/load'
+require "bundler/setup"
+Bundler.require
+
+require_relative '../lib/cli.rb'
+require_relative '../lib/api.rb'
+require_relative '../lib/photos.rb'
---
+++ lib/cli.rb
@@ -0,0 +1,12 @@
+class CLI
+
+     def initialize
+     end
+
+     def welcome
+          puts "Welcome to Pexel."
+
+          puts "Please type animals, nature, or scenes."
+     end
+
+end

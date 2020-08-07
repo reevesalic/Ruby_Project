4df662545d56b04099f01fb8d6a47eb47b10bc2b
reevesalic <reevesalic@gmail.com>
2020-08-07 17:15:45 -0400

    started next level in cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,26 +5,42 @@ class CLI
     puts "\n                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^"
     puts "                     Welcome to Breaking Bad!!".magenta.bold
     puts "\n                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^"
-   
-    # Scraper.new.create_breaking
     Breaking.all
     options
+    choose_option
     end
 
-    def options 
+  def options 
     puts "\nChoose 1 for cast information, 2 for episode information, or 3 to exit!".green.bold
     input = gets.strip
-  case input
-    when "1"
-    puts Scraper.new.actors
-  when "2"
-    puts Scraper.new.episodes
-  when "3"
-    exit
-  else
+    case input
+      when "1"
+     Scraper.new.actors
+     choose_option
+      when "2"
+     Scraper.new.episodes
+     choose_option
+      when "3"
+      exit
+    else
       puts "\nInvalid entry. Please try again.".red.bold
       welcome
     end
+  end
+end
+
+      def choose_option
+        puts "\nPlease choose a number to see more information."
+        input = gets.strip.to_i
+        max_value = Breaking.all.length
+        if input.between?(1,max_value) #checks to see if value is valid
+        else 
+          puts "\nPlease enter a valid input".red.bold
+          options
+          choose_option
       end
     end
+    
+  
+
   
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,28 +12,40 @@ end
 def actors
     site = "https://www.amc.com/shows/breaking-bad/cast-crew"
     website = Nokogiri::HTML(open(site))
-    actors = website.css(".person-content h2")
+    actors = website.css('h2')[0, 4]
+    actors.each.with_index(1) do |actor, index|
+    puts "\n#{index}. #{actor}".chomp("h2")
     actors
 end
+end
+
 
 def episodes
   site = "https://www.amc.com/shows/breaking-bad/full-episodes"
   website = Nokogiri::HTML(open(site))
-  episodes = website.css("div.title").text
+  episodes = website.css("div.title")[0, 4]
+  episodes.each.with_index(1) do |episode, index|
+    puts "\n#{index}. #{episode}"
   episodes
   end
+end
 
 def episode_details
   site = "https://www.amc.com/shows/breaking-bad/full-episodes"
   website = Nokogiri::HTML(open(site))
   episodes_info = website.css(".description")
+  episodes_info.each.with_index(1) do |episode_info, index|
+    puts "\n#{index}. #{episode_info}"
 episode_details
 end
-
+end
 def cast_details
   site = "https://www.amc.com/shows/breaking-bad/cast-crew"
   website = Nokogiri::HTML(open(site))
   cast_info = website.css("cast-link")
+  cast_info.each.with_index(1) do |cast_info, index|
+    puts "\n#{index}. #{cast_info}"
 cast_details
+end
 end
 end

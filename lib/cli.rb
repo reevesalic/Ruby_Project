c93b626144a1dc6f90102d3679c4666e77a9dde2
reevesalic <reevesalic@gmail.com>
2020-08-14 12:53:25 -0400

    Project completegit add .

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,25 +5,37 @@ class CLI
     puts "\n                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^"
     options
   end
+
+  def goodbye
+    exit!
+  end
+
+  def validation
+    
+      puts "Please enter a valid number.".red.bold
+    
+  end
   def options 
     question = Scraper.new.question #first level scrape baptism questions
-
-    puts "\nPlease choose a number to learn more about baptism or type exit!\n".green.bold
-    input = ""
+    puts "\nPlease choose a number to learn more about baptism or type 5 to exit!\n".green.bold
     input = gets.strip.to_i
-    max_value = Baptism.all.length
-    if input.between?(1, max_value)
+    
+    
+    if input.to_i == 5
+      goodbye
+    elsif input.to_i.between?(1, 5)
       baptism = Baptism.all[input.to_i-1]
-      
       puts baptism.answer
+      
       puts "\nPlease visit https://www.navigate.church/ for additional information\n".blue
       options
-    elsif input == "exit"
-      exit
-    else
-      puts "Please enter a valid number.".red.bold
+     
+    else !(input.to_i.between?(1, 5))
+      
+      validation
       options
     end
  end
+
 end
 
